1858e87987ddb012f8f41142b50656ba1ed2e91b
tier3guy <gavinash6290@gmail.com>
2024-07-22 21:09:11 +0530

    fix: vercel configuration

---
+++ build/src/app.js
@@ -0,0 +1,7 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Express on Vercel"));
+export default app;
+//# sourceMappingURL=app.js.map
