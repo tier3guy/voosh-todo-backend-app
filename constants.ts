1f2a6c6082da676c46abdb268992557d6b56de7e
tier3guy <gavinash6290@gmail.com>
2024-07-23 01:01:20 +0530

    fix: PORT issue resolved

--- constants.ts
+++ constants.ts
@@ -2,9 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const APP_MODE: "DEV" | "PROD" = "DEV";
-
-export const PORT = APP_MODE === "DEV" ? 8080 : process.env.PORT!;
+export const PORT = process.env.PORT || 8080;
 
 export const JWT_SECRET = process.env.JWT_SECRET!;
 export const COOKIE_NAME = process.env.COOKIE_NAME!;
