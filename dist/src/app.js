bd20ac3ee6f1ae80084c18797cd6ca3132eef34e
tier3guy <gavinash6290@gmail.com>
2024-07-22 20:55:43 +0530

    init: project setup

--- dist/src/app.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-const app = express();
-app.use(bodyParser.json());
-app.get("/", (req, res) => res.send("Express on Vercel"));
-export default app;
-//# sourceMappingURL=app.js.map
--- dist/src/index.js
+++
@@ -1,6 +0,0 @@
-import app from "./app.js";
-import { PORT } from "../constants.js";
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-//# sourceMappingURL=index.js.map
