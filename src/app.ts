226f6d78500335ab3b0584b21d64a1e0a65cf8b9
tier3guy <gavinash6290@gmail.com>
2024-07-23 12:11:52 +0530

    fix: cors resolution

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 // Middlewares
 app.use(
     cors({
-        origin: ["http://localhost:3000", "https://localhost:3000"],
+        origin: "*",
     })
 );
 app.use(bodyParser.json());
