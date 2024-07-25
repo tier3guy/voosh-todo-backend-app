e033274421c24cbd128c088d4e8cee01a2b18944
tier3guy <gavinash6290@gmail.com>
2024-07-25 18:45:43 +0530

    feat: update order

--- build/src/routes/todo.js
+++ build/src/routes/todo.js
@@ -4,11 +4,13 @@ import getController from "../controllers/todo/get.controller.js";
 import createController from "../controllers/todo/create.controller.js";
 import updateController from "../controllers/todo/update.controller.js";
 import deleteController from "../controllers/todo/delete.controller.js";
+import updateTaskOrder from "../controllers/todo/updateTaskOrder.js";
 const router = Router();
 router.get("/all", allController);
 router.get("/:id", getController);
 router.post("/create", createController);
 router.put("/:id", updateController);
+router.put("/all", updateTaskOrder);
 router.delete("/:id", deleteController);
 export default router;
 //# sourceMappingURL=todo.js.map
