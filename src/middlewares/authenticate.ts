67c7823ffc4797e73d207e0a288aa65c5758322e
tier3guy <gavinash6290@gmail.com>
2024-07-25 09:33:57 +0530

    fix: vercel json schema fixes

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -8,7 +8,7 @@ export default async function authenticate(
     res: Response,
     next: NextFunction
 ) {
-    const { token } = req.body;
+    const token = req.headers.authorization?.split(" ")[1];
 
     if (!token) {
         return res.status(401).send("Access denied. No token provided.");
